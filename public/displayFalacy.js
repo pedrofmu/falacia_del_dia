c5762893ace2d7277d8cdef257d94dc46a333f85
pedrofm3000 <pedrofm@pedrofm.dev>
2023-12-27 16:24:04 +0100

    chrome appearence and aspect ratio improved

--- public/displayFalacy.js
+++ public/displayFalacy.js
@@ -1,34 +1,36 @@
 const falacy_frase = document.getElementById("falacy_phrase");
 const falacy_definition = document.getElementById("falacy_definition");
-const falacy_ejemplification = document.getElementById("falacy_ejemplification");
+const falacy_ejemplification = document.getElementById(
+  "falacy_ejemplification"
+);
 
-fetch('/api/')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Hubo un problema al obtener los datos.');
-        }
-        return response.json();
-    })
-    .then(data => {    
-        i = 0;
-        let falacyArray = data.frase.split(''); 
-        falacyArray.forEach(element => {
-            let leter = document.createElement("span");
-            if (element != " "){
-                leter.innerHTML = element.toUpperCase(); 
-            }else{
-                leter.innerHTML = "&middot;";
-            }
-            leter.style = "--i:" + i;
-            i++;
-            falacy_frase.appendChild(leter); // Agregar cada h1 a falacy_frase
-        });
-        falacy_definition.innerHTML = data.definicion;
-        falacy_ejemplification.innerHTML = data.ejemplificacion;
-    })
-    .catch(error => {
-        // Manejar cualquier error que haya ocurrido durante la solicitud
-        console.error('Hubo un problema con la solicitud fetch:', error);
+falacy_frase.innerHTML = "";
+
+fetch("/api/")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Hubo un problema al obtener los datos.");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    i = 0;
+    let falacyArray = data.frase.split("");
+    falacyArray.forEach((element) => {
+      let leter = document.createElement("span");
+      if (element != " ") {
+        leter.innerHTML = element.toUpperCase();
+      } else {
+        leter.innerHTML = "&middot;";
+      }
+      leter.style = "--i:" + i;
+      i++;
+      falacy_frase.appendChild(leter); // Agregar cada h1 a falacy_frase
     });
-
-
+    falacy_definition.innerHTML = data.definicion;
+    falacy_ejemplification.innerHTML = data.ejemplificacion;
+  })
+  .catch((error) => {
+    // Manejar cualquier error que haya ocurrido durante la solicitud
+    console.error("Hubo un problema con la solicitud fetch:", error);
+  });
